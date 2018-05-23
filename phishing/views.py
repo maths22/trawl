441b152451a806e295811dcef13a5f96b0e703a7
Jacob Burroughs <maths22@gmail.com>
2018-05-22 20:45:51 -0500

    Initial submission template complete

--- phishing/views.py
+++ phishing/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the phishing index")
+    template = loader.get_template('phishing/index.html')
+    context = {
+        'testing': 'hi there',
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def submit_template(request):
+    template = loader.get_template('phishing/submit.html')
+    context = {
+        'directions': 'TODO',
+    }
+    return HttpResponse(template.render(context, request))
