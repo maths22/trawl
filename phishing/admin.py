b7285a44f734af8e29bb6d3b373629334dbbbca5
Claude Richoux <laudecay@gmail.com>
2018-05-23 03:15:19 +0000

    adding models to admin site

--- phishing/admin.py
+++ phishing/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import MTurkUser, Submission, Constraint, Rating
+
+admin.site.register(MTurkUser)
+admin.site.register(Rating)
+admin.site.register(Constraint)
+admin.site.register(Submission) 
